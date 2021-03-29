738b3f24a12947320c5a7600e40ae08ac159fff2
Frizlab <fload@me.com>
2021-03-29 10:32:46 +0200

    Bump stream reader

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.1"),
-		.package(url: "https://github.com/Frizlab/stream-reader.git", from: "3.0.0-rc.5")
+		.package(url: "https://github.com/Frizlab/stream-reader.git", from: "3.0.0")
 	],
 	targets: [
 		.target(name: "ApplyFileSharingACLs", dependencies: [
